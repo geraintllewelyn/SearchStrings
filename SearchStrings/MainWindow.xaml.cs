72eeaa8b3dc87db093375b3d02f462b1d589a252
Geraint Llewelyn <utslao@live.warwick.ac.uk>
2020-03-06 07:54:45 +0000

    Add project files.

---
+++ SearchStrings/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+
+
+namespace SearchStrings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtQuery.Text = "";
+            lstAND.Items.Clear();
+            lstNOT.Items.Clear();
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+
+            var SearchObject = new ProcessString(txtQuery.Text);
+            SearchObject.Validate();
+            if (SearchObject.ANDlist.Count > 0) 
+            {
+                foreach (string s in SearchObject.ANDlist)
+                {
+                    lstAND.Items.Add(s);
+                }
+            }
+
+            if (SearchObject.NOTlist.Count > 0)
+            {
+                foreach (string s in SearchObject.NOTlist)
+                {
+                    lstNOT.Items.Add(s);
+                }
+            }
+
+            
+
+            
+
+        }
+    }
+}
---
+++ SearchStrings/ProcessString.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SearchStrings
+{
+    class ProcessString
+    {
+
+        private string _Searchstring;
+
+        public List<string> NOTlist { get; set; }
+        public List<string> ANDlist { get; set; }
+        
+
+        public ProcessString(string Searchstring)
+        {
+            _Searchstring = Searchstring;
+            ANDlist = new List<string>();
+            NOTlist = new List<string>();
+
+        }
+
+        private string Cleanstring(string Uncleanstring)
+        {
+            Uncleanstring = Regex.Replace(Uncleanstring, "[()]", ""); //remove brackets
+            return Uncleanstring.Trim();
+
+        }
+
+        private List<string> Splitter(string Searchstring, string Pattern)
+        {
+            var Searches = new List<string>();
+            var Matches = Regex.Matches(Searchstring, Pattern);
+
+            if (Matches.Count > 0)
+            {
+                var startindex = 0;
+
+                //first substring - upto the first occurence of the pattern
+                Searches.Add(Cleanstring(Searchstring.Substring(startindex, Matches[0].Index)));
+                startindex = Matches[0].Index + Matches[0].Length;
+
+                //Add middle AND statements if they exsist
+                for (int i = 1; i < Matches.Count; i++)
+                {
+                    Searches.Add(Cleanstring(Searchstring.Substring(startindex, Matches[i].Index - startindex)));
+                    startindex = Matches[i].Index + Matches[i].Length;
+                }
+
+                //last substring - after the last occurence of the pattern
+                Searches.Add(Cleanstring(Searchstring.Substring(startindex)));
+            }
+            else
+            {
+                Searches.Add(Cleanstring(Searchstring));
+            }
+
+            return Searches;
+
+        }
+
+        public void Validate()
+        {
+            var NOTpattern = @" NOT ";
+            var ANDpattern = @" AND ";
+
+            var NOTmatches = Regex.Matches(_Searchstring, NOTpattern);
+
+            if (NOTmatches.Count > 0)
+            {
+                 NOTlist = Splitter(_Searchstring.Substring(NOTmatches[0].Index + NOTmatches[0].Length), NOTpattern);
+                _Searchstring = _Searchstring.Substring(0, NOTmatches[0].Index); 
+            }
+
+            ANDlist = Splitter(_Searchstring, ANDpattern);
+        }
+
+
+
+
+
+    }
+}
